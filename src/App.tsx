5c55f88c0b0299f5a6b162f549408baf09411173
ansonYam <y.anson@hotmail.com>
2023-04-04 00:56:57 -0700

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import {
@@ -10,6 +10,7 @@ import {
 import { observer } from "@legendapp/state/react";
 import { fromSpecObservable } from './from-spec-observable';
 import './App.css'
+import { event } from '@legendapp/state';
 
 const user = fromSpecObservable(userClient.observable)
 const orgUser = fromSpecObservable(userClient.orgUser.observable)
@@ -27,7 +28,6 @@ function App() {
   useEffect(() => {
     const subscription = orgClient.observable.subscribe({
       next: (org) => {
-        // console.log(org); 
         // undefined, something wrong here - need to use WSL instead of Windows
         setOrgId(org?.id);
       },
@@ -38,6 +38,8 @@ function App() {
     })
     return () => subscription.unsubscribe()
   })
+
+  const sleep = (ms: any) => new Promise(r => setTimeout(r, ms));
 
   const handleLanguageChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
     const newLanguage = event.target.value;
@@ -51,52 +53,75 @@ function App() {
 
     // TODO: move the localhost url to an env file, change for production
     try {
-      // Get the current URL, hardcoded for now
-      const stream_url = 'https://www.youtube.com/watch?v=ahLiJuj6_tM';
+      // Get the current URL, hardcoded for now, needs to be a livestream
+      let stream_url = 'https://www.youtube.com/watch?v=hj0YykikOkM';
 
-      setEventSource(new EventSource(`http://localhost:5000/start?stream_url=${stream_url}&to_code=${selectedLanguage}`));
-      if (eventSource) {
-        eventSource.onmessage = (event) => {
-          const data = JSON.parse(event.data);
-          // console.log(data);
-          setTranscription(prevTranscription => {
-            // Keep only the last 3 lines of transcription
-            const newTranscription = [...prevTranscription, data.transcription].slice(-3);
-            return newTranscription;
-          });
-
-          setTranslation(prevTranslation => {
-            const newTranslation = [...prevTranslation, data.translation].slice(-3)
-            return newTranslation;
-          });
-        }
+      // start the audio and transcription threads in the backend
+      let response = await fetch(`http://localhost:5000/start?stream_url=${stream_url}&to_code=${selectedLanguage}`);
+      console.log("Fetch response: ", response);
+      if (response.ok) {
+        eventSource?.close(); // just in case
+        setEventSource(new EventSource(`http://localhost:5000/stream`));  
+      } else {
+        throw new Error('Failed to start the audio thread');
       }
     } catch (error) {
       console.error(error);
     }
   };
 
-  const stopTranscription = () => {
-    fetch('http://localhost:5000/stop');
+  const stopTranscription = async () => {
     if (eventSource) {
       eventSource.close();
       setEventSource(null);
     }
-  }
+    let response = await fetch('http://localhost:5000/stop');
+    if (!response.ok) {
+      throw new Error('Failed to stop the threads');
+    }
+  };
 
   useEffect(() => {
-    console.log("New eventSource: ", eventSource);
+    if (eventSource) {
+      console.log("New eventSource: ", eventSource);
+      eventSource.onmessage = (event) => {
+        let data = JSON.parse(event.data);
+        // console.log(data);
+        setTranscription(prevTranscription => {
+          // Keep only the last 3 lines of transcription
+          let newTranscription = [...prevTranscription, data.transcription].slice(-3);
+          return newTranscription;
+        });
+
+        setTranslation(prevTranslation => {
+          let newTranslation = [...prevTranslation, data.translation].slice(-3)
+          return newTranslation;
+        });
+      };
+    }
+
     return () => {
       if (eventSource) {
-        eventSource.close();
+        eventSource.close()
       }
     }
   }, [eventSource]);
 
   useEffect(() => {
-    console.log("New language: ", selectedLanguage);
-    stopTranscription();
-    startTranscription();
+    if (eventSource) {
+      console.log("New language: ", selectedLanguage);
+
+      const restart = async () => {
+        try {
+          await stopTranscription();
+          await startTranscription();
+    
+        } catch (error) {
+          console.error(error);
+        }
+      }
+      restart();
+    }
   }, [selectedLanguage]);
 
   return (
@@ -109,10 +134,10 @@ function App() {
         <option value="fr">French</option>
       </select>
       <br />
-      <button onClick={startTranscription}>Start transcription</button>
-      <button onClick={stopTranscription}>Stop transcription</button>
       <p>{transcription}</p>
       <p>{translation}</p>
+      <button onClick={startTranscription}>Start transcription</button>
+      <button onClick={stopTranscription}>Stop transcription</button>
     </div>
   )
 }
